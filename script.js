efebb5bd973893848b3b043e8b33b96bd5a8ab4f
Amrryzen <alvinnoval001@gmail.com>
2025-03-14 03:13:27 +0700

    a

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
+// Inisialisasi index slide saat ini
 let currentIndex = 0;
-const totalSlides = document.querySelectorAll('.product').length;
+const totalSlides = document.querySelectorAll('.product').length; // Hitung jumlah slide
 const carouselContainer = document.querySelector('.carousel-container');
 
 // Fungsi untuk mengupdate posisi carousel
 function updateCarousel() {
-  carouselContainer.style.transform = `translateX(-${currentIndex * (160 / totalSlides)}%)`;
+  carouselContainer.style.transform = `translateX(-${currentIndex * (100 / totalSlides)}%)`;
 }
 
-// Pergeseran slide setiap 3 detik
-const slideInterval = setInterval(() => {
+// Pindah slide otomatis setiap 3 detik
+setInterval(() => {
   currentIndex = (currentIndex + 1) % totalSlides;
   updateCarousel();
-}, 5000);
-
-// Reset carousel ke slide pertama setiap 10 detik
-const resetInterval = setInterval(() => {
-  currentIndex = 0;
-  updateCarousel();
-}, 15000);
+}, 3000);
